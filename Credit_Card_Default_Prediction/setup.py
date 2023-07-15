1a619ef0dc404181119befc5c4d4a1767d1e7cc1
singhwalia98 <reshabsingh786@gmail.com>
2023-07-15 14:15:16 +0530

    first commit

---
+++ Credit_Card_Default_Prediction/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To predict the customers who are going to default their payment.',
+    author='Reshab Singh',
+    license='MIT',
+)

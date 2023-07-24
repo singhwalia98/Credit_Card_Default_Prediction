1c8e67a561256dabf3989342bcb2e04fdcf5630f
singhwalia98 <reshabsingh786@gmail.com>
2023-07-24 18:35:46 +0530

    DVC Pipeline

--- src/models/production_model_selection.py
+++ src/models/production_model_selection.py
@@ -1,60 +1,63 @@
 import joblib
 import mlflow
-import logging
 import argparse
-import sys
 from pprint import pprint
-from mlflow.sklearn import load_model
 from train_model import read_params
 from mlflow.tracking import MlflowClient
-from src.exception import CustomException
-from src.logger import logging
 
-def log_production_model(config_path, mlflow_config, max_accuracy_run_id):
+def log_production_model(config_path):
+    config = read_params(config_path)
+    mlflow_config = config["mlflow_config"]
+    remote_server_uri = mlflow_config["remote_server_uri"]
+    
+    mlflow.set_tracking_uri(remote_server_uri)
+
     try:
-        config = read_params(config_path)
-        model_name = mlflow_config["registered_model_name"]
-        model_dir = config["model_dir"]
-
-        mlflow.set_tracking_uri(mlflow_config["remote_server_uri"])
-        client = MlflowClient()
-        logged_model = None
-
-        for mv in client.search_model_versions(f"name='{model_name}'"):
-            mv = dict(mv)
-            if mv["run_id"] == max_accuracy_run_id:
-                current_version = mv["version"]
-                logged_model = mv["source"]
-                pprint(mv, indent=4)
-                client.transition_model_version_stage(name=model_name, version=current_version, stage="Production")
-            else:
-                current_version = mv["version"]
-                client.transition_model_version_stage(name=model_name, version=current_version, stage="Staging")
-
-        if logged_model is not None:
-            model = load_model(logged_model)
-            print("Model loaded successfully from MLflow.")
-            print("Saving model to:", model_dir)  # Debugging line
+        client = mlflow.tracking.MlflowClient()
+        runs = client.search_runs(experiment_ids=mlflow.get_experiment_by_name(mlflow_config["experiment_name"]).experiment_id)
+        
+        # Filter runs that have the accuracy metric logged
+        runs_with_accuracy = [r for r in runs if 'accuracy' in r.data.metrics]
+        
+        # Find the run with the maximum accuracy
+        if runs_with_accuracy:
+            best_run = max(runs_with_accuracy, key=lambda r: r.data.metrics['accuracy'])
+            best_run_id = best_run.info.run_id
+            
+            # Load the model from the best run
+            model_uri = f"runs:/{best_run_id}/model"
+            model = mlflow.sklearn.load_model(model_uri)
+            
+            # Set the stage of the best model to "Production" and other models to "Staging"
+            for mv in client.search_model_versions(f"name='{mlflow_config['registered_model_name']}'"):
+                mv = dict(mv)
+                if mv["run_id"] == best_run_id:
+                    current_version = mv["version"]
+                    client.transition_model_version_stage(
+                        name=mlflow_config["registered_model_name"],
+                        version=current_version,
+                        stage="Production"
+                    )
+                else:
+                    current_version = mv["version"]
+                    client.transition_model_version_stage(
+                        name=mlflow_config["registered_model_name"],
+                        version=current_version,
+                        stage="Staging"
+                    )
+            
+            # Save the best model to disk in the specified directory
+            model_dir = config["model_dir"]
             joblib.dump(model, model_dir)
-            print("Model saved successfully using Joblib.")  # Debugging line
-            logging.info('We have found our best Model and have been dumped successfully using Joblib in the Models dir')
+        
+        else:
+            print("No runs found with accuracy metric. Please make sure the 'accuracy' metric is logged during training.")
 
     except Exception as e:
-        print("Exception occurred while saving the model:", e)  # Debugging line
-        logging.error('Exception occurred while choosing the Best model and dumping it in Models dir')
-        raise CustomException(e, sys)
+        print("Error occurred while loading the production model:", e)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-
-    config = read_params(parsed_args.config)
-    mlflow_config = config["mlflow_config"]
-
-    mlflow.set_tracking_uri(mlflow_config["remote_server_uri"])
-    runs = mlflow.search_runs(experiment_ids=[1])
-    max_accuracy = max(runs["metrics.accuracy"])
-    max_accuracy_run_id = list(runs[runs["metrics.accuracy"] == max_accuracy]["run_id"])[0]
-
-    log_production_model(parsed_args.config, mlflow_config, max_accuracy_run_id)
+    log_production_model(config_path=parsed_args.config)
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,16 +1,23 @@
-import json
-import yaml
 import joblib
 import mlflow
 import argparse
 import numpy as np
 import pandas as pd
 import sys
+import yaml
 from src.logger import logging
 from src.exception import CustomException
 from urllib.parse import urlparse
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score, recall_score, accuracy_score, precision_score, confusion_matrix, classification_report
+from mlflow.pyfunc import PythonModel
+
+class CustomRandomForestModel(PythonModel):
+    def __init__(self, model):
+        self.model = model
+
+    def predict(self, context, model_input):
+        return self.model.predict(model_input)
 
 def read_params(config_path):
     try:
@@ -20,78 +27,79 @@ def read_params(config_path):
     
     except Exception as e:
         logging.error('Some exception happened while reading the parameters from the yaml file.')
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
 
-def accuracymeasures(y_test,predictions,avg_method):
+def accuracymeasures(y_test, predictions, avg_method):
     """
     This function has been defined to measure all the Performance matrices.
     Input: True data & Predicted Data 
-    Output: Different matrix scores, Confusion Matrix and Classification Report
+    Output: Different matrix scores, Confusion Matrix, and Classification Report
     """
     try:
         accuracy = accuracy_score(y_test, predictions)
         precision = precision_score(y_test, predictions, average=avg_method)
         recall = recall_score(y_test, predictions, average=avg_method)
         f1score = f1_score(y_test, predictions, average=avg_method)
-        target_names = ['0','1']
+        target_names = ['0', '1']
+
         print("Classification report")
-        print("---------------------","\n")
-        print(classification_report(y_test, predictions,target_names=target_names),"\n")
+        print("---------------------", "\n")
+        print(classification_report(y_test, predictions, target_names=target_names), "\n")
+
         print("Confusion Matrix")
-        print("---------------------","\n")
-        print(confusion_matrix(y_test, predictions),"\n")
+        print("---------------------", "\n")
+        print(confusion_matrix(y_test, predictions), "\n")
 
         print("Accuracy Measures")
-        print("---------------------","\n")
+        print("---------------------", "\n")
         print("Accuracy: ", accuracy)
         print("Precision: ", precision)
         print("Recall: ", recall)
         print("F1 Score: ", f1score)
 
         logging.info(f"We have received the scores --> accuracy: {accuracy}, precision: {precision}, recall: {recall}, f1score: {f1score}")
-        return accuracy,precision,recall,f1score
+        return accuracy, precision, recall, f1score
     
     except Exception as e:
-        logging.error('Some exception occured while checking different accuracy scores of the Model')
-        raise CustomException(e,sys)
+        logging.error('Some exception occurred while checking different accuracy scores of the Model')
+        raise CustomException(e, sys)
 
-def get_feat_and_target(df,target):
+def get_feat_and_target(df, target):
     """
-    Get features and target variables seperately from given dataframe and target 
+    Get features and target variables separately from given dataframe and target 
     input: dataframe and target column
     output: two dataframes for x and y 
     """
     try:
-        x=df.drop(target,axis=1)
-        y=df[[target]]
-        return x,y
+        x = df.drop(target, axis=1)
+        y = df[target].values.ravel()
+        return x, y
     
     except Exception as e:
-        raise CustomException(e,sys) 
+        raise CustomException(e, sys) 
 
 def train_and_evaluate(config_path):
-
     config = read_params(config_path)
     train_data_path = config["processed_data_config"]["train_data_csv"]
     test_data_path = config["processed_data_config"]["test_data_csv"]
     target = config["raw_data_config"]["target"]
-    max_depth=config["random_forest"]["max_depth"]
-    min_samples_leaf=config["random_forest"]["min_samples_leaf"]
-    min_samples_split=config["random_forest"]["min_samples_split"]
-    n_estimators=config["random_forest"]["n_estimators"]
+    max_depth = config["random_forest"]["max_depth"]
+    min_samples_leaf = config["random_forest"]["min_samples_leaf"]
+    min_samples_split = config["random_forest"]["min_samples_split"]
+    n_estimators = config["random_forest"]["n_estimators"]
 
     try:
         train = pd.read_csv(train_data_path, sep=",")
         test = pd.read_csv(test_data_path, sep=",")
-        train_x,train_y=get_feat_and_target(train,target)
-        test_x,test_y=get_feat_and_target(test,target)
-        logging.info('We have successfully seggregated the Independent & Dependent variables')
+        train_x, train_y = get_feat_and_target(train, target)
+        test_x, test_y = get_feat_and_target(test, target)
+        logging.info('We have successfully segregated the Independent & Dependent variables')
 
     except Exception as e:
-        logging.error('We have received some error while seggregating dependent and independent features')
-        raise CustomException(e,sys)
+        logging.error('We have received some error while segregating dependent and independent features')
+        raise CustomException(e, sys)
 
-################### `MLFLOW` ###############################
+    ################### MLFLOW ###############################
 
     mlflow_config = config["mlflow_config"]
     remote_server_uri = mlflow_config["remote_server_uri"]
@@ -100,14 +108,17 @@ def train_and_evaluate(config_path):
     mlflow.set_experiment(mlflow_config["experiment_name"])
 
     try:
-
-        with mlflow.start_run(run_name=mlflow_config["run_name"],nested=True) as mlops_run:
-            model = RandomForestClassifier(max_depth=max_depth,min_samples_leaf=min_samples_leaf, min_samples_split=min_samples_split,n_estimators=n_estimators)
+        with mlflow.start_run():
+            model = RandomForestClassifier(max_depth=max_depth, min_samples_leaf=min_samples_leaf,
+                                           min_samples_split=min_samples_split, n_estimators=n_estimators)
             model.fit(train_x, train_y)
             y_pred = model.predict(test_x)
-            accuracy,precision,recall,f1score = accuracymeasures(test_y,y_pred,'weighted')
+            accuracy = accuracy_score(test_y, y_pred)
+            precision = precision_score(test_y, y_pred, average="weighted")
+            recall = recall_score(test_y, y_pred, average="weighted")
+            f1score = f1_score(test_y, y_pred, average="weighted")
 
-            mlflow.log_param("max_depth",max_depth)
+            mlflow.log_param("max_depth", max_depth)
             mlflow.log_param("min_samples_leaf", min_samples_leaf)
             mlflow.log_param("min_samples_split", min_samples_split)
             mlflow.log_param("n_estimators", n_estimators)
@@ -116,19 +127,22 @@ def train_and_evaluate(config_path):
             mlflow.log_metric("precision", precision)
             mlflow.log_metric("recall", recall)
             mlflow.log_metric("f1_score", f1score)
-        
-            tracking_url_type_store = urlparse(mlflow.get_artifact_uri()).scheme
+
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
             if tracking_url_type_store != "file":
-                mlflow.sklearn.log_model(model, "model", registered_model_name=mlflow_config["registered_model_name"])
+                mlflow.sklearn.log_model(
+                    model,
+                    "model",
+                    registered_model_name=mlflow_config["registered_model_name"])
             else:
                 mlflow.sklearn.log_model(model, "model")
-    
+
     except Exception as e:
-        logging.error('Some error occured while running the experiments using MLFLOW')
-        raise CustomException(e,sys)
+        logging.error('Some error occurred while running the experiments using MLFLOW')
+        raise CustomException(e, sys)
  
-if __name__=="__main__":
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
